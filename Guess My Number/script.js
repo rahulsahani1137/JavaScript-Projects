cdd490e6963d612c361616a445539b4e3d536a95
Rahul Sahani <yourahul1917@gmail.com>
2024-10-02 23:20:24 +0530

    Update project configuration and styles

---
+++ Guess My Number/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = "🎉 Correct Number!";
+
+document.querySelector('.score').textContent = 13
+document.querySelector('.number').textContent = 20
+
+document.querySelector('.guess').value=25
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function() {
+    console.log(document.querySelector('.guess').value);
+    
+})
